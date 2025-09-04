ac63936d3dae79ac6969ba9d00d2395a0740b879
SebastianRonce <jronceria@ucundinamarca.edu.co>
2025-09-03 23:56:27 -0500

    Delete Hola.py

--- Hola.py
+++
@@ -1,9 +0,0 @@
-def main():
-    a = int(input("Ingrese el primer numero: "))
-
-    b = int(input("Ingrese el segundo numero: "))
-
-    print(f"{a} + {b} = {a + b}")
-
-if __name__ == "__main__":
-    main()

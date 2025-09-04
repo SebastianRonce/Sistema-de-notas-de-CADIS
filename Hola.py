602fd322e9ecec9c08663e2fd302c173fe9b781a
SebastianRonce <jronceria@ucundinamarca.edu.co>
2025-09-03 23:55:53 -0500

    Add files via upload

---
+++ Hola.py
@@ -0,0 +1,9 @@
+def main():
+    a = int(input("Ingrese el primer numero: "))
+
+    b = int(input("Ingrese el segundo numero: "))
+
+    print(f"{a} + {b} = {a + b}")
+
+if __name__ == "__main__":
+    main()
